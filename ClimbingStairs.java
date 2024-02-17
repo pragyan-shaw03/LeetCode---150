5d6927e33b5f0fd076a2a4c4c73692970dcb81ab
Pragyan Shaw <pragyanwork101@gmail.com>
2024-02-17 17:36:17 +0530

    sec com

---
+++ ClimbingStairs.java
@@ -0,0 +1,8 @@
+class ClimbingStairs {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        int a = 10;
+        int c = 30;
+        System.out.println(a+c);
+    }
+}
