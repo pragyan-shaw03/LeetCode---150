e27194c2e5ae3dcbbc9b91024ed7a67bb6efb87c
Pragyan Shaw <pragyanwork101@gmail.com>
2024-02-17 17:37:55 +0530

    third com

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,8 +1,5 @@
 class ClimbingStairs {
     public static void main(String[] args) {
         System.out.println("Hi");
-        int a = 10;
-        int c = 30;
-        System.out.println(a+c);
     }
 }
